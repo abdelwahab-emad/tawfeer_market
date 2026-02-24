d50861e1006fdc8c77fc9217bb5bb60db6129749
AbdelwahabEmad <abd916587@gmail.com>
2026-02-24 23:39:54 +0200

    build splash screen

---
+++ lib/splash_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  static const String id = 'splash_page';
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
