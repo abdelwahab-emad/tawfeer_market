8728f506ce12fb8c99ef5cc355af4098447abd24
AbdelwahabEmad <abd916587@gmail.com>
2026-04-17 15:18:25 +0200

    feat : build search bar in products page using custom text field

--- lib/widgets/products_view.dart
+++ lib/widgets/products_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tawfeer_market/widgets/custom_text_field.dart';
 
 class ProductsView extends StatelessWidget {
   const ProductsView({super.key});
@@ -7,6 +8,29 @@ class ProductsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SearchBar(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomTextField(
+      labelText: 'Search Products...',
+      prefixIcon: Icons.search_rounded,
+      borderRadius: 20,
+      
     );
   }
 }
